6dafc9effd2d4a272a07b1dd494fd722cd576ccb
chengjiang <15712859926@163.com>
2018-10-24 13:24:35 +0800

    在node中使用模板引擎

--- 02/code/05-在node中使用模板引擎.js
+++ 02/code/05-在node中使用模板引擎.js
@@ -18,5 +18,44 @@
 // 3.查文档，使用模板引擎的API
 
 var template = require('art-template');
+var fs = require('fs');
 // 这里不是浏览器
 // template('script标签id'，{对象})
+
+/* var tplStr = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>Document</title>
+</head>
+<body>
+<p>大家好,我叫: {{ name }}</p>
+<p>我今年{{ age }}岁了</p>
+<h1>我来自{{ province }}</h1>
+<p>我喜欢: {{each hobbies}} {{ $value }} {{/each}}</p>
+</body>
+</html>
+` */
+fs.readFile('./tpl.html',function(err,data){
+    if(err){
+        return console.log('读取文件失败了');
+    }
+    // 默认读取到的data是二进制数据
+    // 而模板引擎的render方法需要接受的是字符串
+    // 所以我们在这里需要把data二进制数据转为字符串才可以给模板引擎使用
+    var ret = template.render(data.toString(), {
+        name: 'Jack',
+        age: 18,
+        province: '北京市',
+        hobbies: [
+            '写代码',
+            '唱歌',
+            '打游戏'
+        ],
+        title:'个人信息'
+    })
+    console.log(ret);
+})
