431ae564e6cc1b4c8a784bcdb0209172ec991ac1
chengjiang <15712859926@163.com>
2018-10-25 10:03:44 +0800

    走通了首页与发表评论页切换与404处理

--- 02/code/feedback/app.js
+++ 02/code/feedback/app.js
@@ -3,6 +3,9 @@
 // 为了让目录结构保持统一清晰,所以我们约定把所有的HTML文件都放到views(视图)目录中
 // 我们为了方便的统一处理这些静态资源,所以我们约定把所有的静态资源都存放在public目录中
 // 哪些资源能被用户访问,哪些资源不能被用户访问，我现在可以通过代码来进行非常灵活的访问
+//  / index.html
+// /public 整个public目录中的资源都允许被访问
+// 前后端融会贯通了，为所欲为
 
 var http = require('http');
 var fs = require('fs');
@@ -17,6 +20,13 @@ http
                 }
                 res.end(data);
             })
+        }else if(url === '/post'){
+            fs.readFile('./views/post.html', function (err, data) {
+                if (err) {
+                    return res.end('404 Not Found.')
+                }
+                res.end(data)
+            })
         } else if (url.indexOf('/public/') === 0) {
             //  /public/css/main.css
             //  /public/js/main.js
@@ -24,8 +34,16 @@ http
             // 统一处理
             // 如果请求路径是以/public/开头的,则我认为你要获取public中的某个资源
             // 所以我们就直接可以把请求路径当做文件路径来直接进行读取
-            fs.readFile('.'+url,function(err,data){
-                if(err){
+            fs.readFile('.' + url, function (err, data) {
+                if (err) {
+                    return res.end('404 Not Found.')
+                }
+                res.end(data)
+            })
+        } else {
+            // 其它的都处理成404 找不到
+            fs.readFile('./views/404.html', function (err, data) {
+                if (err) {
                     return res.end('404 Not Found.')
                 }
                 res.end(data)
