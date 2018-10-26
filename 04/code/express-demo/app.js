4ea0dfd6c00346e7ede141608e0895a124b8a3bd
chengjiang <15712859926@163.com>
2018-10-26 11:11:25 +0800

    nodemon工具自动重启服务

--- 04/code/express-demo/app.js
+++ 04/code/express-demo/app.js
@@ -15,7 +15,20 @@ app.get('/login',function(req,res){
     res.write('world ')
     res.end() */
     // res.end('hello world');
-    res.send('login page')
+    res.send(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Document</title>
+    </head>
+    <body>
+        <h1>hello login aaa</h1>
+    </body>
+    </html>
+    `)
 })
 
 app.listen(3000,function(){
--- 04/code/readme.md
+++ 04/code/readme.md
@@ -16,3 +16,18 @@ npm install --save express
 hello world
 
 #### 修改完代码自动重启
+1. 我们这里可以使用一个第三方命令行工具:`nodemon`来帮我们解决频繁修改代码重启服务器问题.
+2. `nodemon`是一个基于Node.js开发的一个第三方命令行工具,我们使用的时候需要独立安装.
+```javascript
+# 在任意目录执行该命令都可以
+# 也就是说,所有需要 --global来安装的包都可以在任意目录执行
+`npm install --global nodemon`
+```
+3. 安装完毕之后,使用.
+    ```javascript
+         node app.js
+
+        # 使用nodemon
+        nodemon app.js
+    ```
+只要是通过`nodemon app.js`启动的服务,则它会监视你的文件变化.当文件发生变化的时候,自动帮你重启服务器.
