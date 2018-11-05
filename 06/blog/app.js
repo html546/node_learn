27795f148f61c8af7aebb50536939a6145ff677b
chengjiang <15712859926@163.com>
2018-11-05 09:06:26 +0800

    art-template中的include-extend-block语法

--- 06/blog/app.js
+++ 06/blog/app.js
@@ -6,8 +6,15 @@ var app = express()
 app.use('/public/', express.static(path.join(__dirname, './public/')))
 app.use('/node_modules/', express.static(path.join(__dirname, './node_modules/')))
 
+// 在Node中,有很多第三方模板引擎都可以使用,不是只有art-template
+// ejs、jade(pug)、handlebars、nunjucks
+app.engine('html', require('express-art-template'));
+app.set('views',path.join(__dirname,'./views/')) //默认就是 ./views目录
+
 app.get('/',function(req,res){
-    res.send('hello')
+    res.render('index.html',{
+        name:'张三'
+    })
 })
 
 app.listen(3000,function(){
